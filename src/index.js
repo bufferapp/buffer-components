f73aac31bd1c6ada4d7d91e8fef03174f29d273e
Harrison Harnisch <hharnisc@gmail.com>
2016-09-07 11:28:06 -0500

    Export components

---
+++ src/index.js
@@ -0,0 +1,7 @@
+export { default as Card } from './Card';
+export { default as Icon } from './Icon';
+export { default as Image } from './Image';
+export { default as NavBar } from './NavBar';
+export { default as Text } from './Text';
+export { default as VectorGraphic } from './VectorGraphic';
+export { default as Video } from './Video';
