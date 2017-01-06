0bb1386b222896c1338e84d33d355d466e313107
Steve Dixon <steve@bufferapp.com>
2017-01-06 19:29:16 +0000

    Add new 'failed' state for Card component
    This commit adds a new 'failed' state to the Card component. This is
    future-proofing the Card component when we start to build out more
    complex Post components that will utilize this state. :D

--- Card/story.js
+++ Card/story.js
@@ -4,17 +4,20 @@ import Card from './index';
 
 storiesOf('Card')
   .add('Default', () => (
-    <Card>A Regular Old Card</Card>
+    <Card>What is a Product Designer? An awesome story by <a href="#">@jgadapee</a> over on Medium! <a href="#">http://buff.ly/1LTbUqv</a></Card>
   ))
   .add('doublePadding', () => (
-    <Card doublePadding>A Card With Extra Padding</Card>
+    <Card doublePadding>What is a Product Designer? An awesome story by <a href="#">@jgadapee</a> over on Medium! <a href="#">http://buff.ly/1LTbUqv</a></Card>
   ))
   .add('empty', () => (
-    <Card empty>An Empty Old Card</Card>
+    <Card empty>Nothing to see here! 👻</Card>
+  ))
+  .add('failed', () => (
+    <Card failed>What is a Product Designer? An awesome story by <a href="#">@jgadapee</a> over on Medium! <a href="#">http://buff.ly/1LTbUqv</a></Card>
   ))
   .add('noBorder', () => (
-    <Card noBorder>A Borderless Old Card</Card>
+    <Card noBorder>What is a Product Designer? An awesome story by <a href="#">@jgadapee</a> over on Medium! <a href="#">http://buff.ly/1LTbUqv</a></Card>
   ))
   .add('noPadding', () => (
-    <Card noPadding>A Card With No Padding</Card>
+    <Card noPadding>What is a Product Designer? An awesome story by <a href="#">@jgadapee</a> over on Medium! <a href="#">http://buff.ly/1LTbUqv</a></Card>
   ));
