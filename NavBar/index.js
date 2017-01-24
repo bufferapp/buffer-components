0a61e127113de62aa73c5360992f8445063b3dce
Harrison Harnisch <hharnisc@gmail.com>
2017-01-24 16:41:47 -0600

    NavBar uses CSS modules

--- NavBar/index.js
+++ NavBar/index.js
@@ -1,47 +1,21 @@
 import React, { PropTypes } from 'react';
-import reactCSS from 'reactcss';
 import Icon from '../Icon';
 import Text from '../Text';
+import { classNames } from '../lib/utils';
+import styles from './style.css';
 
-const NavBar = ({ title, subtitle }) => {
-  const styles = reactCSS({
-    default: {
-      NavBar: {
-        alignItems: 'center',
-        flexDirection: 'row',
-        display: 'flex',
-        marginBottom: 32,
-        padding: '0 16px',
-        width: '100%',
-        height: 64,
-        backgroundColor: '#fff',
-        borderBottom: '1px solid #ced7df',
-        boxSizing: 'border-box',
-      },
-      AppIcon: {
-        marginRight: 8,
-        display: 'flex',
-      },
-    },
-  });
-  return (
-    <div
-      style={styles.NavBar}
-    >
-      <div
-        style={styles.AppIcon}
-      >
-        <Icon
-          type={'buffer'}
-          height={15}
-          width={15}
-        />
-      </div>
-      <Text weight={'bold'}>{title}</Text>
-      <Text>{subtitle ? `\u00a0${subtitle}` : ''}</Text>
+const NavBar = ({ title, subtitle }) =>
+  <div className={classNames(styles, 'navbar')}>
+    <div className={classNames(styles, 'app-icon')}>
+      <Icon
+        type={'buffer'}
+        height={15}
+        width={15}
+      />
     </div>
-  );
-};
+    <Text weight={'bold'}>{title}</Text>
+    <Text>{subtitle ? `\u00a0${subtitle}` : ''}</Text>
+  </div>;
 
 NavBar.propTypes = {
   subtitle: PropTypes.string,
