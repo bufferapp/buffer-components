0ab21e0d1b3922ffe12a560007d4ce24d51aebe5
Harrison Harnisch <hharnisc@gmail.com>
2017-01-25 17:17:16 -0600

    Clean up NavBar

--- NavBar/index.js
+++ NavBar/index.js
@@ -1,18 +1,11 @@
 import React, { PropTypes } from 'react';
 import Icon from '../Icon';
 import Text from '../Text';
-import { classNames } from '../lib/utils';
 import styles from './style.css';
 
 const NavBar = ({ title, subtitle }) =>
-  <div className={classNames(styles, 'navbar')}>
-    <div className={classNames(styles, 'app-icon')}>
-      <Icon
-        type={'buffer'}
-        height={15}
-        width={15}
-      />
-    </div>
+  <div className={styles.navbar}>
+    <Icon type={'buffer'} className={styles['app-icon']} />
     <Text weight={'bold'}>{title}</Text>
     <Text>{subtitle ? `\u00a0${subtitle}` : ''}</Text>
   </div>;
