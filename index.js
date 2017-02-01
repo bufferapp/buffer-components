6b44e989eb52d2fd78eac7fa813ba38f606d7fbd
Harrison Harnisch <hharnisc@gmail.com>
2017-02-01 11:34:34 -0600

    export Icon, Image, Link, LinkfiedText, Text

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
+// if you need a component export it here
 export Button from './Button';
 export Card from './Card';
-// export Icon from './Icon';
-// export Image from './Image';
-// export NavBar from './NavBar';
-// export Text from './Text';
-// export VectorGraphic from './VectorGraphic';
-// export Video from './Video';
-// export { TextColors } from './shared/colors';
+export Icon from './Icon';
+export Image from './Image';
+export Link from './Link';
+export LinkifiedText from './LinkifiedText';
+export Text from './Text';
