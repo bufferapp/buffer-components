833fa20352eea421a8f5bd5e5ed0185bcdc01e1b
Harrison Harnisch <hharnisc@gmail.com>
2017-02-03 16:09:00 -0600

    use Link in Card stories

--- Card/story.jsx
+++ Card/story.jsx
@@ -1,23 +1,24 @@
 import React from 'react';
 import { storiesOf } from '@kadira/storybook';
 import Card from './index';
+import Link from '../Link';
 
 storiesOf('Card')
   .add('Default', () => (
-    <Card>What is a Product Designer? An awesome story by <a href="#">@jgadapee</a> over on Medium! <a href="#">http://buff.ly/1LTbUqv</a></Card>
+    <Card>What is a Product Designer? An awesome story by <Link href={'#'}>@jgadapee</Link> over on Medium! <Link href={'#'}>http://buff.ly/1LTbUqv</Link></Card>
   ))
   .add('doublePadding', () => (
-    <Card doublePadding>What is a Product Designer? An awesome story by <a href="#">@jgadapee</a> over on Medium! <a href="#">http://buff.ly/1LTbUqv</a></Card>
+    <Card doublePadding>What is a Product Designer? An awesome story by <Link href={'#'}>@jgadapee</Link> over on Medium! <Link href={'#'}>http://buff.ly/1LTbUqv</Link></Card>
   ))
   .add('empty', () => (
     <Card empty>Nothing to see here! 👻</Card>
   ))
   .add('failed', () => (
-    <Card failed>What is a Product Designer? An awesome story by <a href="#">@jgadapee</a> over on Medium! <a href="#">http://buff.ly/1LTbUqv</a></Card>
+    <Card failed>What is a Product Designer? An awesome story by <Link href={'#'}>@jgadapee</Link> over on Medium! <Link href={'#'}>http://buff.ly/1LTbUqv</Link></Card>
   ))
   .add('noBorder', () => (
-    <Card noBorder>What is a Product Designer? An awesome story by <a href="#">@jgadapee</a> over on Medium! <a href="#">http://buff.ly/1LTbUqv</a></Card>
+    <Card noBorder>What is a Product Designer? An awesome story by <Link href={'#'}>@jgadapee</Link> over on Medium! <Link href={'#'}>http://buff.ly/1LTbUqv</Link></Card>
   ))
   .add('noPadding', () => (
-    <Card noPadding>What is a Product Designer? An awesome story by <a href="#">@jgadapee</a> over on Medium! <a href="#">http://buff.ly/1LTbUqv</a></Card>
+    <Card noPadding>What is a Product Designer? An awesome story by <Link href={'#'}>@jgadapee</Link> over on Medium! <Link href={'#'}>http://buff.ly/1LTbUqv</Link></Card>
   ));
