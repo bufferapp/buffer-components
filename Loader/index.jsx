57cd1f108cc8e69a4da9d386daca43794e3d731c
Steve Dixon <steve@bufferapp.com>
2017-02-03 16:43:59 +0000

    Remove utils import and use {styles} instead
    This commit removes the need to use the `classNames` utility. Removing
    this gives us a single value that we're returning so removing the return
    statement and {} syntax on our arrow function feels super clean! :D

--- Loader/index.jsx
+++ Loader/index.jsx
@@ -1,22 +1,17 @@
 import React, { PropTypes } from 'react';
-import { classNames } from '../lib/utils';
 import Icon from '../Icon';
 import Text from '../Text';
 import styles from './style.css';
 
-const Loader = ({ children }) => {
-  const classes = classNames(styles, 'loader');
-  return (
-    <div className={classes}>
-      <div className={styles.icon}>
-        <Icon type={'buffer-top'} className={styles.top} />
-        <Icon type={'buffer-middle'} className={styles.middle} />
-        <Icon type={'buffer-bottom'} className={styles.bottom} />
-      </div>
-      <Text>{children}</Text>
+const Loader = ({ children }) =>
+  <div className={styles.loader}>
+    <div className={styles.icon}>
+      <Icon type={'buffer-top'} className={styles.top} />
+      <Icon type={'buffer-middle'} className={styles.middle} />
+      <Icon type={'buffer-bottom'} className={styles.bottom} />
     </div>
-  );
-};
+    <Text>{children}</Text>
+  </div>;
 
 Loader.propTypes = {
   children: PropTypes.node,
