48923d68bb564453abe67e38b46edb3038121335
Harrison Harnisch <hharnisc@gmail.com>
2017-02-03 22:39:50 +0000

    Hoverable card (#26)
    * use Link in Card stories
    
    * hovered is a prop on Card
    
    * style Link component
    
    * implement onMouseEnter(Leave) on Card

--- Card/story.jsx
+++ Card/story.jsx
@@ -1,23 +1,41 @@
 import React from 'react';
-import { storiesOf } from '@kadira/storybook';
+import {
+  storiesOf,
+  action,
+} from '@kadira/storybook';
 import Card from './index';
+import Link from '../Link';
 
 storiesOf('Card')
-  .add('Default', () => (
-    <Card>What is a Product Designer? An awesome story by <a href="#">@jgadapee</a> over on Medium! <a href="#">http://buff.ly/1LTbUqv</a></Card>
+  .add('default', () => (
+    <Card>What is a Product Designer? An awesome story by <Link href={'#'}>@jgadapee</Link> over on Medium! <Link href={'#'}>http://buff.ly/1LTbUqv</Link></Card>
   ))
   .add('doublePadding', () => (
-    <Card doublePadding>What is a Product Designer? An awesome story by <a href="#">@jgadapee</a> over on Medium! <a href="#">http://buff.ly/1LTbUqv</a></Card>
+    <Card doublePadding>What is a Product Designer? An awesome story by <Link href={'#'}>@jgadapee</Link> over on Medium! <Link href={'#'}>http://buff.ly/1LTbUqv</Link></Card>
   ))
   .add('empty', () => (
     <Card empty>Nothing to see here! 👻</Card>
   ))
   .add('failed', () => (
-    <Card failed>What is a Product Designer? An awesome story by <a href="#">@jgadapee</a> over on Medium! <a href="#">http://buff.ly/1LTbUqv</a></Card>
+    <Card failed>What is a Product Designer? An awesome story by <Link href={'#'}>@jgadapee</Link> over on Medium! <Link href={'#'}>http://buff.ly/1LTbUqv</Link></Card>
   ))
   .add('noBorder', () => (
-    <Card noBorder>What is a Product Designer? An awesome story by <a href="#">@jgadapee</a> over on Medium! <a href="#">http://buff.ly/1LTbUqv</a></Card>
+    <Card noBorder>What is a Product Designer? An awesome story by <Link href={'#'}>@jgadapee</Link> over on Medium! <Link href={'#'}>http://buff.ly/1LTbUqv</Link></Card>
   ))
   .add('noPadding', () => (
-    <Card noPadding>What is a Product Designer? An awesome story by <a href="#">@jgadapee</a> over on Medium! <a href="#">http://buff.ly/1LTbUqv</a></Card>
+    <Card noPadding>What is a Product Designer? An awesome story by <Link href={'#'}>@jgadapee</Link> over on Medium! <Link href={'#'}>http://buff.ly/1LTbUqv</Link></Card>
+  ))
+  .add('hovered', () => (
+    <Card hovered>What is a Product Designer? An awesome story by <Link href={'#'}>@jgadapee</Link> over on Medium! <Link href={'#'}>http://buff.ly/1LTbUqv</Link></Card>
+  ))
+  .add('empty + hovered', () => (
+    <Card empty hovered>What is a Product Designer? An awesome story by <Link href={'#'}>@jgadapee</Link> over on Medium! <Link href={'#'}>http://buff.ly/1LTbUqv</Link></Card>
+  ))
+  .add('onMouseEnter + onMouseLeave', () => (
+    <Card
+      onMouseEnter={action('on-mouse-enter')}
+      onMouseLeave={action('on-mouse-leave')}
+    >
+      What is a Product Designer? An awesome story by <Link href={'#'}>@jgadapee</Link> over on Medium! <Link href={'#'}>http://buff.ly/1LTbUqv</Link>
+    </Card>
   ));
