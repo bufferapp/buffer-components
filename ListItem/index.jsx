bd0d7d424bf793a1d9df59bb2f570dcd056d54d6
Harrison Harnisch <hharnisc@gmail.com>
2017-02-07 15:18:28 -0600

    remove bullet from li

--- ListItem/index.jsx
+++ ListItem/index.jsx
@@ -1,7 +1,8 @@
 import React, { PropTypes } from 'react';
+import style from './style.css';
 
 const ListItem = ({ children }) =>
-  <li>{children}</li>;
+  <li className={style.li}>{children}</li>;
 
 ListItem.propTypes = {
   children: PropTypes.node,
