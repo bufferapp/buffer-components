ead09f5034177a9267a11447bb6cda551aaa7bc6
Steve Dixon <steve@buffer.com>
2017-02-08 17:26:00 +0000

    Clean up Overlay story by removing Button

--- Overlay/story.jsx
+++ Overlay/story.jsx
@@ -1,11 +1,8 @@
 import React from 'react';
 import { storiesOf } from '@kadira/storybook';
 import Overlay from './index';
-import Button from '../Button/index';
 
 storiesOf('Overlay')
   .add('default', () => (
-    <Overlay>
-      <Button>Test</Button>
-    </Overlay>
+    <Overlay />
   ));
