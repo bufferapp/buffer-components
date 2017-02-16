d6d76beb4f3a3f7c46cbf0c49e91271581ebb504
Steve Dixon <steve@buffer.com>
2017-02-16 22:20:12 +0000

    Update story to test out AddMedia icon

--- Icon/story.jsx
+++ Icon/story.jsx
@@ -1,27 +1,8 @@
 import React from 'react';
 import { storiesOf } from '@kadira/storybook';
-import Icon from './index';
+import AddMedia from './Icons/AddMedia';
 
 storiesOf('Icon')
-  .add('buffer', () => (
-    <Icon
-      type={'buffer'}
-    />
-  ))
-  .add('circle-twitter', () => (
-    <Icon
-      type={'circle-twitter'}
-    />
-  ))
-  .add('small', () => (
-    <Icon
-      type={'buffer'}
-      size={'small'}
-    />
-  ))
-  .add('large', () => (
-    <Icon
-      type={'buffer'}
-      size={'large'}
-    />
+  .add('addMedia', () => (
+    <AddMedia size={'large'} />
   ));
