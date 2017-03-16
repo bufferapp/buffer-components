e349026f2894239a8a9e672e4e008df0a0baa28d
Steve Dixon <stevemdixon@users.noreply.github.com>
2017-03-16 14:07:38 +0000

    [WIP] MultipleImages component (#39)
    * Implement initial component structure
    
    * cleanup and a small sizing fix
    
    * Add defaultProps and stories
    
    * Add function to detect number of image URLs
    
    This will then adjust the height of the `imageWrapper` container
    accordingly.
    
    * Remove redundant CSS
    
    Cleans up our CSS file by removing an unused class.
    
    * Pass test
    
    * Update snapshot to pass
    
    * Rename function to be more descriptive
    
    Using `getImageWrapperHeight` and passing in a parameter of
    `arrayOfImageUrls` feels more descriptive and less confusing than
    `getUrlAmount` and passing in `urls`. :)
    
    * Polish MultipleImages PR
    - container fills up space by default
    - add a test for 1 image
    - add a test to make sure the height can be changed
    
    * Fix typo
    
    * Remove obsolete snapshot

---
+++ MultipleImages/index.jsx
@@ -0,0 +1,62 @@
+import React, { PropTypes } from 'react';
+import uuid from 'uuid';
+import styles from './style.css';
+import Image from '../Image';
+
+const getImageWrapperHeight = (arrayOfImageUrls) => {
+  if (arrayOfImageUrls.length <= 2) {
+    return { height: '100%' };
+  }
+};
+
+const MultipleImages = ({ urls,
+  width,
+  height,
+}) => {
+  const inlineStyle = {
+    height,
+    width,
+  };
+
+  return (
+    <div className={styles.multipleImages} style={inlineStyle}>
+      {
+        urls.map(url =>
+          <div
+            className={styles.imageWrapper}
+            height={height}
+            key={uuid()}
+            style={getImageWrapperHeight(urls)}
+          >
+            <Image
+              height={'100%'}
+              objectFit={'cover'}
+              src={url}
+              verticalAlignBottom
+              width={'100%'}
+            />
+          </div>,
+        )
+      }
+    </div>
+  );
+};
+
+MultipleImages.defaultProps = {
+  height: '100%',
+  width: '100%',
+};
+
+MultipleImages.propTypes = {
+  height: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.number,
+  ]),
+  urls: PropTypes.arrayOf(PropTypes.string),
+  width: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.number,
+  ]),
+};
+
+export default MultipleImages;
---
+++ MultipleImages/story.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import { storiesOf } from '@kadira/storybook';
+import MultipleImages from './index';
+
+storiesOf('MultipleImages')
+  .add('fourImages', () => (
+    <MultipleImages
+      width={'20rem'}
+      height={'20rem'}
+      urls={[
+        'http://lorempixel.com/400/400/cats/',
+        'http://lorempixel.com/400/400/cats/',
+        'http://lorempixel.com/400/400/cats/',
+        'http://lorempixel.com/400/400/cats/',
+      ]}
+    />
+  ))
+  .add('threeImages', () => (
+    <MultipleImages
+      width={'20rem'}
+      height={'20rem'}
+      urls={[
+        'http://lorempixel.com/400/400/cats/',
+        'http://lorempixel.com/400/400/cats/',
+        'http://lorempixel.com/400/400/cats/',
+      ]}
+    />
+  ))
+  .add('twoImages', () => (
+    <MultipleImages
+      width={'20rem'}
+      height={'20rem'}
+      urls={[
+        'http://lorempixel.com/400/400/cats/',
+        'http://lorempixel.com/400/400/cats/',
+      ]}
+    />
+  ))
+  .add('oneImage', () => (
+    <MultipleImages
+      width={'20rem'}
+      height={'20rem'}
+      urls={[
+        'http://lorempixel.com/400/400/cats/',
+      ]}
+    />
+  ))
+  .add('width=30rem,height=30rem', () => (
+    <MultipleImages
+      width={'30rem'}
+      height={'30rem'}
+      urls={[
+        'http://lorempixel.com/400/400/cats/',
+        'http://lorempixel.com/400/400/cats/',
+        'http://lorempixel.com/400/400/cats/',
+        'http://lorempixel.com/400/400/cats/',
+      ]}
+    />
+  ));
