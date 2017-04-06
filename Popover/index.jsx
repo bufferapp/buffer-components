c34f626f6bbba78c27b6f32a1a15348d11c8f414
Harrison Harnisch <hharnisc@gmail.com>
2017-04-06 13:54:50 -0500

    handle overlay clicks

--- Popover/index.jsx
+++ Popover/index.jsx
@@ -2,16 +2,17 @@ import React, { PropTypes } from 'react';
 import styles from './style.css';
 import Overlay from '../Overlay';
 
-const Popover = ({ children }) =>
+const Popover = ({ children, onOverlayClick }) =>
   <span>
     <div className={styles.popover}>
       {children}
     </div>
-    <Overlay />
+    <Overlay onClick={onOverlayClick} />
   </span>;
 
 Popover.propTypes = {
   children: PropTypes.node,
+  onOverlayClick: PropTypes.func,
 };
 
 export default Popover;
--- Popover/story.jsx
+++ Popover/story.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { storiesOf } from '@kadira/storybook';
+import { storiesOf, action } from '@kadira/storybook';
 import { checkA11y } from 'storybook-addon-a11y';
 import Popover from './index';
 import Card from '../Card';
@@ -7,7 +7,7 @@ import Card from '../Card';
 storiesOf('Popover')
   .addDecorator(checkA11y)
   .add('default', () => (
-    <Popover>
+    <Popover onOverlayClick={action('overlay-click')}>
       <Card>Popover</Card>
     </Popover>
   ));
