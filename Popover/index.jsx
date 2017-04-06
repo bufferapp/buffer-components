5f7b49bb3e38ce1e7dfb4bcb38dd835b7d7dfdc1
Harrison Harnisch <hharnisc@gmail.com>
2017-04-06 13:42:16 -0500

    render default Popover in middle of screen

--- Popover/index.jsx
+++ Popover/index.jsx
@@ -1,5 +1,13 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
+import styles from './style.css';
 
-const Popover = () => <div>Popover</div>;
+const Popover = ({ children }) =>
+  <div className={styles.popover}>
+    {children}
+  </div>;
+
+Popover.propTypes = {
+  children: PropTypes.node,
+};
 
 export default Popover;
--- Popover/story.jsx
+++ Popover/story.jsx
@@ -2,9 +2,12 @@ import React from 'react';
 import { storiesOf } from '@kadira/storybook';
 import { checkA11y } from 'storybook-addon-a11y';
 import Popover from './index';
+import Card from '../Card';
 
 storiesOf('Popover')
   .addDecorator(checkA11y)
   .add('default', () => (
-    <Popover />
+    <Popover>
+      <Card>Popover</Card>
+    </Popover>
   ));
