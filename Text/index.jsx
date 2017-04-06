4b371b166d060c445be292a1c553125b428c14a3
Steve Dixon <steve@buffer.com>
2017-04-06 12:27:57 +0100

    Alphabetize properties and configs

--- Text/index.jsx
+++ Text/index.jsx
@@ -4,15 +4,15 @@ import styles from './style.css';
 
 const Text = ({ children, size, weight, color }) => {
   const classes = classNames(styles, 'text', {
-    extraSmall: size === 'extra-small',
-    large: size === 'large',
-    small: size === 'small',
-    bold: weight === 'bold',
-    thin: weight === 'thin',
     black: color === 'black',
     blue: color === 'blue',
+    bold: weight === 'bold',
+    extraSmall: size === 'extra-small',
     gray: color === 'gray',
+    large: size === 'large',
     red: color === 'red',
+    small: size === 'small',
+    thin: weight === 'thin',
     white: color === 'white',
   });
   return (
@@ -22,7 +22,7 @@ const Text = ({ children, size, weight, color }) => {
 
 Text.propTypes = {
   children: PropTypes.node,
-  size: PropTypes.oneOf(['extra-small', 'large', 'small']),
+  size: PropTypes.oneOf(['extra-small', 'large', 'mini', 'small']),
   weight: PropTypes.oneOf(['bold', 'thin']),
   color: PropTypes.oneOf(['black', 'blue', 'gray', 'red', 'white']),
 };
--- Text/story.jsx
+++ Text/story.jsx
@@ -10,32 +10,32 @@ storiesOf('Text')
   .add('Default', () => (
     <Text>{text}</Text>
   ))
-  .add('ExtraSmall', () => (
-    <Text size={'extra-small'}>{text}</Text>
+  .add('Black', () => (
+    <Text color={'black'}>{text}</Text>
   ))
-  .add('Large', () => (
-    <Text size={'large'}>{text}</Text>
-  ))
-  .add('Small', () => (
-    <Text size={'small'}>{text}</Text>
+  .add('Blue', () => (
+    <Text color={'blue'}>{text}</Text>
   ))
   .add('Bold', () => (
     <Text weight={'bold'}>{text}</Text>
   ))
-  .add('Thin', () => (
-    <Text weight={'thin'}>{text}</Text>
-  ))
-  .add('black', () => (
-    <Text color={'black'}>{text}</Text>
-  ))
-  .add('blue', () => (
-    <Text color={'blue'}>{text}</Text>
+  .add('ExtraSmall', () => (
+    <Text size={'extra-small'}>{text}</Text>
   ))
   .add('Gray', () => (
     <Text color={'gray'}>{text}</Text>
   ))
+  .add('Large', () => (
+    <Text size={'large'}>{text}</Text>
+  ))
   .add('Red', () => (
     <Text color={'red'}>{text}</Text>
+  ))
+  .add('Small', () => (
+    <Text size={'small'}>{text}</Text>
+  ))
+  .add('Thin', () => (
+    <Text weight={'thin'}>{text}</Text>
   ))
   .add('White', () => (
     <Text color={'white'}>{text}</Text>
