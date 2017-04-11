c43afd084c5b2366bd9825a95336a62ee0216d9e
Harrison Harnisch <hharnisc@gmail.com>
2017-04-10 21:35:18 -0500

    set value on text input

--- InputText/index.jsx
+++ InputText/index.jsx
@@ -1,6 +1,12 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 
-const InputText = () =>
-  <div>InputText</div>;
+const InputText = ({ input }) =>
+  <input value={input.value} />;
+
+InputText.propTypes = {
+  input: PropTypes.shape({
+    value: PropTypes.string,
+  }),
+};
 
 export default InputText;
--- InputText/story.jsx
+++ InputText/story.jsx
@@ -3,8 +3,12 @@ import { storiesOf } from '@kadira/storybook';
 import { checkA11y } from 'storybook-addon-a11y';
 import InputText from './index';
 
+const input = {
+  value: 'hi',
+};
+
 storiesOf('InputText')
   .addDecorator(checkA11y)
   .add('default', () => (
-    <InputText />
+    <InputText input={input} />
   ));
