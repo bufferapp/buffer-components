f2bdc0c5aca279500edec833e1866f0acbd5d7dd
Harrison Harnisch <hharnisc@gmail.com>
2017-04-12 19:23:48 -0500

    stub in InputTime

---
+++ InputTime/story.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { storiesOf } from '@kadira/storybook';
+import { checkA11y } from 'storybook-addon-a11y';
+import InputTime from './index';
+
+storiesOf('InputTime')
+  .addDecorator(checkA11y)
+  .add('default', () => (
+    <InputTime />
+  ));
