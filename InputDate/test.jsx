3d13a0cfe14adcfbe724792554c7dc5c2fb8d18b
Harrison Harnisch <hharnisc@gmail.com>
2017-04-13 11:03:16 -0500

    complete testing for InputDate

--- InputDate/test.jsx
+++ InputDate/test.jsx
@@ -1,4 +1,43 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import InputDate from './index';
+
 describe('InputDate', () => {
-  it('should trigger onChange when active day is clicked', () => {});
-  it('should NOT trigger onChange when inactive day is clicked', () => {});
+  it('should trigger onChange when active day is clicked', () => {
+    const onChange = jest.fn();
+    const wrapper = mount(
+      <InputDate
+        input={{
+          value: '',
+          onChange,
+        }}
+      />,
+    );
+    wrapper
+      .find('.DayPicker-Day')
+      .not('.DayPicker-Day--disabled')
+      .not('.DayPicker-Day--outside')
+      .first()
+      .simulate('click');
+    expect(onChange)
+      .toBeCalled();
+  });
+  it('should NOT trigger onChange when inactive day is clicked', () => {
+    const onChange = jest.fn();
+    const wrapper = mount(
+      <InputDate
+        input={{
+          value: '',
+          onChange,
+        }}
+      />,
+    );
+    wrapper
+      .find('.DayPicker-Day--disabled')
+      .not('.DayPicker-Day--outside')
+      .first()
+      .simulate('click');
+    expect(onChange)
+      .not.toBeCalled();
+  });
 });
