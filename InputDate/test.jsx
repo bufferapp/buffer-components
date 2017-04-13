43044e88e0ff84c8214d61e7a11cf5a69aea0791
Harrison Harnisch <hharnisc@gmail.com>
2017-04-12 19:18:05 -0500

    stub in tests for InputDate

---
+++ InputDate/test.jsx
@@ -0,0 +1,4 @@
+describe('InputDate', () => {
+  it('should trigger onChange when active day is clicked', () => {});
+  it('should NOT trigger onChange when inactive day is clicked', () => {});
+});
