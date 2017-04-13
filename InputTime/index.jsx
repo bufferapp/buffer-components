97c1b81a037574adcc5d09967eb7a07652e91ce1
Harrison Harnisch <hharnisc@gmail.com>
2017-04-13 12:55:44 -0500

    stub in values for InputTime

--- InputTime/index.jsx
+++ InputTime/index.jsx
@@ -1,5 +1,23 @@
 import React from 'react';
 
-const InputTime = () => <div>InputTime</div>;
+// generate array of numbers (inclusive)
+const genArray = (start, end) => [...Array(end + 1).keys()].slice(start);
+const leftPadTimeUnit = timeUnit => (timeUnit < 10 ? `0${timeUnit}` : timeUnit);
+
+const InputTime = () =>
+  <div>
+    <select>
+      {genArray(1, 12).map(hour =>
+        <option key={hour} value={hour}>{leftPadTimeUnit(hour)}</option>)}
+    </select>
+    <select>
+      {genArray(0, 60).map(min =>
+        <option key={min} value={min}>{leftPadTimeUnit(min)}</option>)}
+    </select>
+    <select>
+      <option value="am">am</option>
+      <option value="pm">pm</option>
+    </select>
+  </div>;
 
 export default InputTime;
