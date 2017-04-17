56d11c39cedc1dbd6055df6b38a0eb34a09d4430
Melissa Alvarez <melissa@buffer.com>
2017-04-17 14:06:04 +0100

    Use prop-types package from npm in all components

--- Input/index.jsx
+++ Input/index.jsx
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import { classNames } from '../lib/utils';
 import styles from './style.css';
 import Text from '../Text';
--- InputDate/index.jsx
+++ InputDate/index.jsx
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import DayPicker from 'react-day-picker';
 import styles from './style.css';
 import Text from '../Text';
