d1dcace0459e136693ade2b6fa6af7a1aa61b579
Harrison Harnisch <hharnisc@gmail.com>
2017-05-15 17:38:28 -0500

    migrate Link to JSS

--- Link/index.jsx
+++ Link/index.jsx
@@ -1,7 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { classNames } from '../lib/utils';
-import styles from './style.css';
+import {
+  calculateStyles,
+} from '../lib/utils';
+import {
+  curiousBlue,
+} from '../style/color';
+
+/*
+TODO:
+
+.link:hover {
+  color: var(--denim);
+}
+
+.link:active {
+  color: var(--tory-blue);
+}
+
+*/
 
 const Link = ({
   children,
@@ -9,12 +26,23 @@ const Link = ({
   newTab,
   unstyled,
 }) => {
-  const classes = classNames(styles, 'link', {
+  const style = calculateStyles({
+    default: {
+      color: curiousBlue,
+    },
+    unstyled: {
+      textDecoration: 'none',
+    },
+  }, {
     unstyled,
   });
 
   return (
-    <a className={classes} href={href} target={newTab ? '_blank' : '_self'}>
+    <a
+      style={style}
+      href={href}
+      target={newTab ? '_blank' : '_self'}
+    >
       {children}
     </a>
   );
