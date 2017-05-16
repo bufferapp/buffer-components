ad83c22ec604d9d856afcd3b17e2d88e29565de8
Harrison Harnisch <hharnisc@gmail.com>
2017-05-15 20:36:09 -0500

    migrate Overlay to JSS

--- Overlay/index.jsx
+++ Overlay/index.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './style.css';
+import {
+  outerSpaceUltraDark,
+} from '../style/color';
+import {
+  overlay,
+} from '../style/zIndex';
 
 const Overlay = ({ onClick, transparent }) =>
   // eslint-disable-next-line jsx-a11y/no-static-element-interactions
@@ -8,6 +14,13 @@ const Overlay = ({ onClick, transparent }) =>
     className={styles.overlay}
     onClick={onClick}
     style={{
+      position: 'absolute',
+      top: 0,
+      right: 0,
+      bottom: 0,
+      left: 0,
+      backgroundColor: outerSpaceUltraDark,
+      zIndex: overlay,
       opacity: transparent ? 0 : 1,
     }}
   />;
