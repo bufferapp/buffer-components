04f105a57707e5341cd4e29d1a0af13e5c2e7777
Harrison Harnisch <hharnisc@gmail.com>
2017-05-18 10:00:12 -0500

    ignore eslint rule

--- style/dropShadow.js
+++ style/dropShadow.js
@@ -1,3 +1,3 @@
 import { outerSpaceLight } from './color';
 
-export const boxShadowLevelOne = `0 1px 2px ${outerSpaceLight}`;
+export const boxShadowLevelOne = `0 1px 2px ${outerSpaceLight}`; // eslint-disable-line import/prefer-default-export
