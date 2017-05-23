5cf47bcdfd3944aba016accdbc5c97bc95f6594f
Harrison Harnisch <hharnisc@gmail.com>
2017-05-22 20:36:09 -0500

    new improved Button

---
+++ Button/test.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import ButtonStateless from '../ButtonStateless';
+import Button from './index';
+
+describe('Button', () => {
+  it('should toggle hovered prop when mouseEnter and mouseLeave are triggered', () => {
+    const wrapper = mount(
+      <Button>A Button</Button>,
+    );
+    const button = wrapper
+      .find(ButtonStateless);
+    button.simulate('mouseEnter');
+    expect(button.props().hovered)
+      .toBe(true);
+    button.simulate('mouseLeave');
+    expect(button.props().hovered)
+      .toBe(false);
+  });
+
+  it('should toggle focused prop when focus and blur are triggered', () => {
+    const wrapper = mount(
+      <Button>A Button</Button>,
+    );
+    const button = wrapper
+      .find(ButtonStateless);
+    button.simulate('focus');
+    expect(button.props().focused)
+      .toBe(true);
+    button.simulate('blur');
+    expect(button.props().focused)
+      .toBe(false);
+  });
+});
