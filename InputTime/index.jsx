ee56388843b0f77849b20fbfec33a8c4a4126fec
Steve Dixon <steve@buffer.com>
2017-05-26 14:32:26 +0100

    Add minimal config and ternary operators to apply styles

--- InputTime/index.jsx
+++ InputTime/index.jsx
@@ -5,27 +5,70 @@ import { calculateStyles } from '../lib/utils';
 const genArray = (start, end) => [...Array(end + 1).keys()].slice(start);
 const leftPadTimeUnit = timeUnit => (timeUnit < 10 ? `0${timeUnit}` : timeUnit);
 
+const style = calculateStyles({
+  default: {
+    display: 'inline-flex',
+  },
+});
+const minimalStyle = calculateStyles({
+  default: {
+    border: 0,
+    background: 'transparent',
+    marginTop: 0,
+    marginRight: '0.25rem',
+    marginBottom: 0,
+    marginLeft: 0,
+    padding: 0,
+    '-webkit-appearance': 'none',
+    '-moz-appearance': 'none',
+  },
+});
+
 /* eslint-disable react/prop-types */
 
-const renderAmPm = ({ value, onChange, submitting }) =>
-  (<select
-    value={value.hours < 12 ? 'am' : 'pm'}
-    disabled={submitting}
-    onChange={e => onChange({
-      ...value,
-      hours: e.target.value === 'am'
-      ? value.hours - 12
-      : value.hours + 12,
-    })}
-  >
-    <option value="am">am</option>
-    <option value="pm">pm</option>
-  </select>);
+const renderAmPm = ({
+  minimal,
+  onChange,
+  submitting,
+  value,
+}) => (
+  <div style={style}>
+    { minimal ?
+      <select
+        disabled={submitting}
+        onChange={e => onChange({
+          ...value,
+          hours: e.target.value === 'AM'
+          ? value.hours - 12
+          : value.hours + 12,
+        })}
+        style={minimalStyle}
+        value={value.hours < 12 ? 'AM' : 'PM'}
+      >
+        <option value="AM">AM</option>
+        <option value="PM">PM</option>
+      </select> :
+      <select
+        disabled={submitting}
+        onChange={e => onChange({
+          ...value,
+          hours: e.target.value === 'AM'
+          ? value.hours - 12
+          : value.hours + 12,
+        })}
+        value={value.hours < 12 ? 'AM' : 'PM'}
+      >
+        <option value="AM">AM</option>
+        <option value="PM">PM</option>
+      </select>
+    }
+  </div>
+  );
 
 /* eslint-enable react/prop-types */
 
 const marginRight = {
-  marginRight: '0.5rem',
+  marginRight: '0.25rem',
 };
 
 const displayHour = (hour, select24Hours) => {
@@ -46,46 +89,73 @@ const InputTime = ({
   meta: {
     submitting,
   },
+  minimal,
   select24Hours,
 }) => {
-  const style = calculateStyles({
-    default: {
-      display: 'inline-flex',
-    },
-  });
   if (!value) {
     value = { hours: 0, minutes: 0 };
   }
   return (
     <div style={style}>
-      <select
-        disabled={submitting}
-        onChange={e => onChange({ ...value, hours: parseInt(e.target.value, 10) })}
-        style={marginRight}
-        value={value.hours}
-      >
-        {genArray(
-          select24Hours || value.hours < 12 ? 0 : 12,
-          select24Hours || value.hours > 11 ? 23 : 11,
-        ).map(hour =>
-          (<option
-            key={hour}
-            value={hour}
-          >
-            {leftPadTimeUnit(displayHour(hour, select24Hours))}
-          </option>))
-        }
-      </select>
-      <select
-        disabled={submitting}
-        onChange={e => onChange({ ...value, minutes: parseInt(e.target.value, 10) })}
-        style={!select24Hours ? marginRight : undefined}
-        value={value.minutes}
-      >
-        {genArray(0, 59).map(min =>
-          <option key={min} value={min}>{leftPadTimeUnit(min)}</option>)}
-      </select>
-      { select24Hours ? null : renderAmPm({ value, onChange, submitting }) }
+      { minimal ?
+        <select
+          disabled={submitting}
+          onChange={e => onChange({ ...value, hours: parseInt(e.target.value, 10) })}
+          style={minimalStyle}
+          value={value.hours}
+        >
+          {genArray(
+            select24Hours || value.hours < 12 ? 0 : 12,
+            select24Hours || value.hours > 11 ? 23 : 11,
+          ).map(hour =>
+            (<option
+              key={hour}
+              value={hour}
+            >
+              {leftPadTimeUnit(displayHour(hour, select24Hours))}
+            </option>))
+          }
+        </select> :
+        <select
+          disabled={submitting}
+          onChange={e => onChange({ ...value, hours: parseInt(e.target.value, 10) })}
+          style={marginRight}
+          value={value.hours}
+        >
+          {genArray(
+            select24Hours || value.hours < 12 ? 0 : 12,
+            select24Hours || value.hours > 11 ? 23 : 11,
+          ).map(hour =>
+            (<option
+              key={hour}
+              value={hour}
+            >
+              {leftPadTimeUnit(displayHour(hour, select24Hours))}
+            </option>))
+          }
+        </select>
+      }
+      { minimal ?
+        <select
+          disabled={submitting}
+          onChange={e => onChange({ ...value, minutes: parseInt(e.target.value, 10) })}
+          style={!select24Hours ? minimalStyle : undefined}
+          value={value.minutes}
+        >
+          {genArray(0, 59).map(min =>
+            <option key={min} value={min}>{leftPadTimeUnit(min)}</option>)}
+        </select> :
+        <select
+          disabled={submitting}
+          onChange={e => onChange({ ...value, minutes: parseInt(e.target.value, 10) })}
+          style={!select24Hours ? marginRight : undefined}
+          value={value.minutes}
+        >
+          {genArray(0, 59).map(min =>
+            <option key={min} value={min}>{leftPadTimeUnit(min)}</option>)}
+        </select>
+      }
+      { select24Hours ? null : renderAmPm({ minimal, onChange, submitting, value }) }
     </div>
   );
 };
@@ -104,6 +174,7 @@ InputTime.propTypes = {
   meta: PropTypes.shape({
     submitting: PropTypes.bool,
   }),
+  minimal: PropTypes.bool,
   select24Hours: PropTypes.bool,
 };
 
