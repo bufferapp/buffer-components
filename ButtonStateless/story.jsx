6101104489e7e2e75700f843bd2ec4d54cc52f18
Melissa Alvarez <melissa@buffer.com>
2017-05-31 16:11:50 +0000

    Add/off white color to button stateless component (#83)
    * quaternary w/ offWhite color to ButtonStateless
    
    * Add quaternary to buttonStateless story
    
    * update snapshot with quaternary
    
    * Add quaternary to disabled config in story
    
    * Update snapshot with quaternary in disabled story

--- ButtonStateless/story.jsx
+++ ButtonStateless/story.jsx
@@ -29,6 +29,8 @@ storiesOf('ButtonStateless')
       <Button disabled small>Button text</Button><br /><br />
       <p>Tertiary</p>
       <Button disabled tertiary>Button text</Button><br /><br />
+      <p>Quaternary</p>
+      <Button disabled quaternary>Button text</Button><br /><br />
       <p>Warning</p>
       <Button disabled warning>Button text</Button>
       <p>Click Events Should Not Fire</p>
@@ -62,6 +64,8 @@ storiesOf('ButtonStateless')
       <Button hovered small>Button text</Button><br /><br />
       <p>Tertiary</p>
       <Button hovered tertiary>Button text</Button><br /><br />
+      <p>Quaternary</p>
+      <Button hovered quaternary>Button text</Button><br /><br />
       <p>Warning</p>
       <Button hovered warning>Button text</Button>
       <p>Disabled</p>
@@ -92,6 +96,9 @@ storiesOf('ButtonStateless')
   ))
   .add('tertiary', () => (
     <Button tertiary>Button text</Button>
+  ))
+  .add('quaternary', () => (
+    <Button quaternary>Button text</Button>
   ))
   .add('warning', () => (
     <Button warning>Button text</Button>
