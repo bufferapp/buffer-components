716c4ce803e4ecc0eebb228d577d4383463e6a9c
Steve Dixon <steve@buffer.com>
2017-06-05 12:04:26 +0100

    Add CloseSmallIcon

---
+++ Icon/Icons/CloseSmallIcon.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import Icon from '../../Icon';
+
+const CloseSmallIcon = ({ color, size }) =>
+  (<Icon color={color} size={size}>
+    <rect
+      height="12"
+      rx="0.5"
+      transform="translate(5.046194, 5.000000) rotate(45.000000) translate(-5.046194, -5.000000)"
+      width="1"
+      x="4.54619413"
+      y="-1"
+    />
+    <rect
+      height="12"
+      rx="0.5"
+      transform="translate(5.046194, 5.000000) scale(-1, 1) rotate(45.000000) translate(-5.046194, -5.000000)"
+      width="1"
+      x="4.54619413"
+      y="-1"
+    />
+  </Icon>);
+
+CloseSmallIcon.propTypes = {
+  ...Icon.propTypes,
+};
+
+export default CloseSmallIcon;
