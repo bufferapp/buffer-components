5765cb5c31fbaf63d11c023ba168cfe1c47b54a4
Harrison Harnisch <hharnisc@gmail.com>
2017-06-13 13:16:55 -0500

    use prop-types package

--- InputTime/index.jsx
+++ InputTime/index.jsx
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import { calculateStyles } from '../lib/utils';
 
 // generate array of numbers (inclusive)
