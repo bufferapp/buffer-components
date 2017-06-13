416e6e281f3bf57f520f166f7d96a29a13fb2ba4
Harrison Harnisch <hharnisc@gmail.com>
2017-06-13 14:51:07 -0500

    update to storybook 3 (#104)

--- ButtonStateless/story.jsx
+++ ButtonStateless/story.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
-import {
-  storiesOf,
-  action,
-} from '@kadira/storybook';
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
 import { checkA11y } from 'storybook-addon-a11y';
 import Button from './index';
 import Text from '../Text';
