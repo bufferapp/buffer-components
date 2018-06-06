bdeea3e3d9189b3512df5a590b9bdc73e021493f
Hamish Macpherson <hamstu@gmail.com>
2018-06-06 11:31:15 -0700

    clean up popover

--- Popover/index.jsx
+++ Popover/index.jsx
@@ -8,6 +8,18 @@ import {
   modal,
 } from '../style/zIndex';
 
+const popoverWrapperStyle = {
+  position: 'absolute',
+  top: 0,
+  left: 0,
+  display: 'flex',
+  flexDirection: 'column',
+  height: '100vh',
+  width: '100vw',
+  justifyContent: 'center',
+  alignItems: 'center',
+};
+
 const Popover = ({
   children,
   left,
@@ -16,61 +28,37 @@ const Popover = ({
   right,
   onOverlayClick,
   transparentOverlay,
-  hidden,
-}) =>
-  (!hidden &&
-    <div style={{
-      position: 'absolute',
-      top: 0,
-      left: 0,
-      display: 'flex',
-      flexDirection: 'column',
-      height: '100vh',
-      width: '100vw',
-      justifyContent: 'center',
-      alignItems: 'center',
-    }}>
-      <div
-        style={calculateStyles({
-          default: {
-            position: 'absolute',
-            left,
-            top,
-            bottom,
-            right,
-            zIndex: modal,
-          },
-          center: {
-            // top: '50%',
-            // left: '50%',
-            // marginLeft: '-25%',
-          },
-        }, {
-          center: !(left || top || bottom || right),
-        })}
-      >
-        {children}
-      </div>
-      <Overlay
-        onClick={onOverlayClick}
-        transparent={transparentOverlay}
-      />
+}) => (
+  <div style={popoverWrapperStyle}>
+    <div
+      style={calculateStyles({
+        default: {
+          position: 'absolute',
+          left,
+          top,
+          bottom,
+          right,
+          zIndex: modal,
+        },
+      })}
+    >
+      {children}
     </div>
-  );
+    <Overlay
+      onClick={onOverlayClick}
+      transparent={transparentOverlay}
+    />
+  </div>
+);
 
 Popover.propTypes = {
-  children: PropTypes.node,
+  children: PropTypes.node.isRequired,
   left: PropTypes.string,
   top: PropTypes.string,
   bottom: PropTypes.string,
   right: PropTypes.string,
   onOverlayClick: PropTypes.func,
   transparentOverlay: PropTypes.bool,
-  hidden: PropTypes.bool,
-};
-
-Popover.defaultProps = {
-  hidden: false,
 };
 
 export default Popover;
