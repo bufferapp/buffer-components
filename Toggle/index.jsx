c35d7c5a1033a90a37b321a1192b4b908e52d5a6
Harrison Harnisch <hharnisc@gmail.com>
2018-06-26 18:05:55 -0500

    Use Text to display label

--- Toggle/index.jsx
+++ Toggle/index.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Text from '../Text';
 
-const Toggle = ({ enabled }) => <div>{ enabled ? 'Enabled' : 'Disabled' }</div>;
+const Toggle = ({ enabled }) =>
+  <Text>{ enabled ? 'Enabled' : 'Disabled' }</Text>;
 
 Toggle.propTypes = {
   enabled: PropTypes.bool,
