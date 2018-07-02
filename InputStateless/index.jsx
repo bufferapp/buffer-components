24d15be62cbe58a4c50fcee11a7017045aa524ba
Harrison Harnisch <hharnisc@gmail.com>
2018-07-02 18:11:07 -0500

    Task/add input text area (#136)
    * Add input text area
    
    * update snapshots

--- InputStateless/index.jsx
+++ InputStateless/index.jsx
@@ -1,19 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { calculateStyles } from '../lib/utils';
-import {
-  fontFamily,
-  fontSize,
-} from '../style/font';
-import {
-  borderRadius,
-  borderWidth,
-} from '../style/border';
-import {
-  geyser,
-  torchRed,
-  curiousBlue,
-} from '../style/color';
+import { fontFamily, fontSize } from '../style/font';
+import { borderRadius, borderWidth } from '../style/border';
+import { geyser, torchRed, curiousBlue } from '../style/color';
 import Text from '../Text';
 
 /* eslint-disable react/prop-types */
@@ -26,21 +16,23 @@ const errorStyle = {
   marginTop: '0.25rem',
 };
 
-const renderLabel = ({ label }) => (
-  label ? (
+const renderLabel = ({ label }) =>
+  (label ? (
     <div style={labelStyle}>
-      <Text size="small" weight="semi-bold">{label}</Text>
+      <Text size="small" weight="semi-bold">
+        {label}
+      </Text>
     </div>
-  ) : null
-);
+  ) : null);
 
-const renderError = ({ error, touched }) => (
-  error && touched ? (
+const renderError = ({ error, touched }) =>
+  (error && touched ? (
     <div style={errorStyle}>
-      <Text size="small" color="torchRed">{error}</Text>
+      <Text size="small" color="torchRed">
+        {error}
+      </Text>
     </div>
-  ) : null
-);
+  ) : null);
 
 /* eslint-enable react/prop-types */
 
@@ -56,43 +48,61 @@ const InputStateless = ({
   onMouseEnter,
   onMouseLeave,
 }) => {
-  const style = calculateStyles({
-    default: {
-      fontFamily,
-      fontSize,
-      padding: '0.5rem',
-      borderRadius,
-      border: `${borderWidth} solid ${geyser}`,
-      borderColor: geyser,
-      width: '100%',
-      boxSizing: 'border-box',
-      outline: 0,
+  const style = calculateStyles(
+    {
+      default: {
+        fontFamily,
+        fontSize,
+        padding: '0.5rem',
+        borderRadius,
+        border: `${borderWidth} solid ${geyser}`,
+        borderColor: geyser,
+        width: '100%',
+        boxSizing: 'border-box',
+        outline: 0,
+      },
+      focused: {
+        borderColor: curiousBlue,
+      },
+      error: {
+        borderColor: torchRed,
+      },
     },
-    focused: {
-      borderColor: curiousBlue,
+    {
+      focused,
+      error: meta.error && meta.touched,
     },
-    error: {
-      borderColor: torchRed,
-    },
-  }, {
-    focused,
-    error: meta.error && meta.touched,
-  });
+  );
   return (
     <div>
       {renderLabel({ label })}
-      <input
-        style={style}
-        disabled={meta.submitting}
-        value={input.value}
-        onChange={input.onChange}
-        placeholder={placeholder}
-        type={type}
-        onFocus={onFocus}
-        onBlur={onBlur}
-        onMouseEnter={onMouseEnter}
-        onMouseLeave={onMouseLeave}
-      />
+      {type === 'textarea' ? (
+        <textarea
+          style={style}
+          disabled={meta.submitting}
+          value={input.value}
+          onChange={input.onChange}
+          placeholder={placeholder}
+          type={type}
+          onFocus={onFocus}
+          onBlur={onBlur}
+          onMouseEnter={onMouseEnter}
+          onMouseLeave={onMouseLeave}
+        />
+      ) : (
+        <input
+          style={style}
+          disabled={meta.submitting}
+          value={input.value}
+          onChange={input.onChange}
+          placeholder={placeholder}
+          type={type}
+          onFocus={onFocus}
+          onBlur={onBlur}
+          onMouseEnter={onMouseEnter}
+          onMouseLeave={onMouseLeave}
+        />
+      )}
       {renderError(meta)}
     </div>
   );
@@ -113,11 +123,7 @@ InputStateless.commonPropTypes = {
 
 InputStateless.propTypes = {
   ...InputStateless.commonPropTypes,
-  type: PropTypes.oneOf([
-    'text',
-    'password',
-    'email',
-  ]),
+  type: PropTypes.oneOf(['text', 'password', 'email', 'textarea']),
   onFocus: PropTypes.func,
   onBlur: PropTypes.func,
   onMouseEnter: PropTypes.func,
