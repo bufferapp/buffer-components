6f45a4b74e5dc541984c486b0ab021252e9417a6
Harrison Harnisch <hharnisc@gmail.com>
2018-07-02 17:24:56 -0500

    make Card background and border customizable

--- Card/story.jsx
+++ Card/story.jsx
@@ -4,53 +4,92 @@ import { action } from '@storybook/addon-actions';
 import { checkA11y } from 'storybook-addon-a11y';
 import Card from './index';
 import Link from '../Link';
+import { denim } from '../style/color';
 
 storiesOf('Card', module)
   .addDecorator(checkA11y)
   .add('default', () => (
-    <Card>What is a Product Designer? An awesome story by <Link href={'#'}>@jgadapee</Link> over on Medium! <Link href={'#'}>http://buff.ly/1LTbUqv</Link></Card>
-  ))
-  .add('doublePadding', () => (
-    <Card doublePadding>What is a Product Designer? An awesome story by <Link href={'#'}>@jgadapee</Link> over on Medium! <Link href={'#'}>http://buff.ly/1LTbUqv</Link></Card>
-  ))
-  .add('empty', () => (
-    <Card empty>Nothing to see here! 👻</Card>
-  ))
-  .add('failed', () => (
-    <Card failed>What is a Product Designer? An awesome story by <Link href={'#'}>@jgadapee</Link> over on Medium! <Link href={'#'}>http://buff.ly/1LTbUqv</Link></Card>
-  ))
-  .add('faded', () => (
-    <Card faded>What is a Product Designer? An awesome story by <Link href={'#'}>@jgadapee</Link> over on Medium! <Link href={'#'}>http://buff.ly/1LTbUqv</Link></Card>
-  ))
-  .add('noBorder', () => (
-    <Card noBorder>What is a Product Designer? An awesome story by <Link href={'#'}>@jgadapee</Link> over on Medium! <Link href={'#'}>http://buff.ly/1LTbUqv</Link></Card>
-  ))
-  .add('noPadding', () => (
-    <Card noPadding>What is a Product Designer? An awesome story by <Link href={'#'}>@jgadapee</Link> over on Medium! <Link href={'#'}>http://buff.ly/1LTbUqv</Link></Card>
-  ))
-  .add('hovered', () => (
-    <Card hovered>What is a Product Designer? An awesome story by <Link href={'#'}>@jgadapee</Link> over on Medium! <Link href={'#'}>http://buff.ly/1LTbUqv</Link></Card>
-  ))
-  .add('empty + hovered', () => (
-    <Card empty hovered>What is a Product Designer? An awesome story by <Link href={'#'}>@jgadapee</Link> over on Medium! <Link href={'#'}>http://buff.ly/1LTbUqv</Link></Card>
-  ))
-  .add('onMouseEnter + onMouseLeave', () => (
-    <Card
-      onMouseEnter={action('on-mouse-enter')}
-      onMouseLeave={action('on-mouse-leave')}
-    >
-      What is a Product Designer? An awesome story by <Link href={'#'}>@jgadapee</Link> over on Medium! <Link href={'#'}>http://buff.ly/1LTbUqv</Link>
+    <Card>
+      What is a Product Designer? An awesome story by <Link href="#">@jgadapee</Link> over on
+      Medium! <Link href="#">http://buff.ly/1LTbUqv</Link>
     </Card>
   ))
-  .add('off-white background color', () => (
-    <Card color={'off-white'}>What is a Product Designer? An awesome story by <Link href={'#'}>@jgadapee</Link> over on Medium! <Link href={'#'}>http://buff.ly/1LTbUqv</Link></Card>
+  .add('doublePadding', () => (
+    <Card doublePadding>
+      What is a Product Designer? An awesome story by <Link href="#">@jgadapee</Link> over on
+      Medium! <Link href="#">http://buff.ly/1LTbUqv</Link>
+    </Card>
+  ))
+  .add('empty', () => <Card empty>Nothing to see here! 👻</Card>)
+  .add('failed', () => (
+    <Card failed>
+      What is a Product Designer? An awesome story by <Link href="#">@jgadapee</Link> over on
+      Medium! <Link href="#">http://buff.ly/1LTbUqv</Link>
+    </Card>
+  ))
+  .add('faded', () => (
+    <Card faded>
+      What is a Product Designer? An awesome story by <Link href="#">@jgadapee</Link> over on
+      Medium! <Link href="#">http://buff.ly/1LTbUqv</Link>
+    </Card>
+  ))
+  .add('noBorder', () => (
+    <Card noBorder>
+      What is a Product Designer? An awesome story by <Link href="#">@jgadapee</Link> over on
+      Medium! <Link href="#">http://buff.ly/1LTbUqv</Link>
+    </Card>
+  ))
+  .add('noPadding', () => (
+    <Card noPadding>
+      What is a Product Designer? An awesome story by <Link href="#">@jgadapee</Link> over on
+      Medium! <Link href="#">http://buff.ly/1LTbUqv</Link>
+    </Card>
+  ))
+  .add('hovered', () => (
+    <Card hovered>
+      What is a Product Designer? An awesome story by <Link href="#">@jgadapee</Link> over on
+      Medium! <Link href="#">http://buff.ly/1LTbUqv</Link>
+    </Card>
+  ))
+  .add('empty + hovered', () => (
+    <Card empty hovered>
+      What is a Product Designer? An awesome story by <Link href="#">@jgadapee</Link> over on
+      Medium! <Link href="#">http://buff.ly/1LTbUqv</Link>
+    </Card>
+  ))
+  .add('onMouseEnter + onMouseLeave', () => (
+    <Card onMouseEnter={action('on-mouse-enter')} onMouseLeave={action('on-mouse-leave')}>
+      What is a Product Designer? An awesome story by <Link href="#">@jgadapee</Link> over on
+      Medium! <Link href="#">http://buff.ly/1LTbUqv</Link>
+    </Card>
+  ))
+  .add('denim background color', () => (
+    <Card background={denim}>
+      What is a Product Designer? An awesome story by <Link href="#">@jgadapee</Link> over on
+      Medium! <Link href="#">http://buff.ly/1LTbUqv</Link>
+    </Card>
   ))
   .add('reducedPadding', () => (
-    <Card reducedPadding>What is a Product Designer? An awesome story by <Link href={'#'}>@jgadapee</Link> over on Medium! <Link href={'#'}>http://buff.ly/1LTbUqv</Link></Card>
+    <Card reducedPadding>
+      What is a Product Designer? An awesome story by <Link href="#">@jgadapee</Link> over on
+      Medium! <Link href="#">http://buff.ly/1LTbUqv</Link>
+    </Card>
   ))
   .add('shadowHeight=1', () => (
-    <Card shadowHeight={1}>What is a Product Designer? An awesome story by <Link href={'#'}>@jgadapee</Link> over on Medium! <Link href={'#'}>http://buff.ly/1LTbUqv</Link></Card>
+    <Card shadowHeight={1}>
+      What is a Product Designer? An awesome story by <Link href="#">@jgadapee</Link> over on
+      Medium! <Link href="#">http://buff.ly/1LTbUqv</Link>
+    </Card>
   ))
   .add('shadowHeight=2', () => (
-    <Card shadowHeight={2}>What is a Product Designer? An awesome story by <Link href={'#'}>@jgadapee</Link> over on Medium! <Link href={'#'}>http://buff.ly/1LTbUqv</Link></Card>
+    <Card shadowHeight={2}>
+      What is a Product Designer? An awesome story by <Link href="#">@jgadapee</Link> over on
+      Medium! <Link href="#">http://buff.ly/1LTbUqv</Link>
+    </Card>
+  ))
+  .add('borderColor=denim', () => (
+    <Card borderColor={denim}>
+      What is a Product Designer? An awesome story by <Link href="#">@jgadapee</Link> over on
+      Medium! <Link href="#">http://buff.ly/1LTbUqv</Link>
+    </Card>
   ));
