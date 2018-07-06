fb0f91a710d2d8ef1099946d60b46378fc2bd650
Hamish Macpherson <hamstu@gmail.com>
2018-07-06 11:15:56 -0700

    update story

--- Input/story.jsx
+++ Input/story.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
 import { checkA11y } from 'storybook-addon-a11y';
 import Input from './index';
 
 storiesOf('Input', module)
   .addDecorator(checkA11y)
   .add('default', () => <Input />)
+  .add('with custom onFocus handler', () => <Input onFocus={action('onFocus')} />)
   .add('type=textarea', () => <Input type="textarea" />);
