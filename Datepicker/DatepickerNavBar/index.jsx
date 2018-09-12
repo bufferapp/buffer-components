b25bb42320b894b9b0acbcb60ca872c5b1ba761b
anafilipadealmeida <anafilipadealmeida@gmail.com>
2018-09-12 15:39:41 +0100

    Add Datepicker NavBar component

---
+++ Datepicker/DatepickerNavBar/index.jsx
@@ -0,0 +1,72 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import DayPicker from 'react-day-picker';
+import Select from '../../Select';
+import { ArrowRightIcon, ArrowLeftIcon } from '../../Icon/Icons';
+import Button from '../../Button';
+import moment from 'moment';
+
+const NavBar = ({
+  month,
+  nextMonth,
+  previousMonth,
+  onPreviousClick,
+  onNextClick,
+  className,
+  localeUtils,
+  firstMonthToDisplay,
+  onNavigationClick,
+}) => {
+  const styles = {
+    left: {
+      float: 'left',
+    },
+    right: {
+      float: 'right',
+    },
+    align: {
+      verticalAlign: 'middle',
+    },
+  };
+
+  const onNext = () => {
+    onNextClick();
+    onNavigationClick(nextMonth);
+  };
+
+  const onPrev = () => {
+    onPreviousClick();
+    onNavigationClick(prevMonth);
+  };
+
+  const disablePreviousButton = (moment(month).isAfter(firstMonthToDisplay, 'month')) ? false : true;
+
+  return (
+    <div className={className}>
+      <div style={styles.left}>
+        <Button noStyle disabled={disablePreviousButton ? true : false} onClick={onPrev}>
+            <span style={styles.align}>
+              <ArrowLeftIcon color={disablePreviousButton ? 'geyser' : 'black'}/>
+            </span>
+        </Button>
+      </div>
+      <div style={styles.right}>
+        <Button noStyle onClick={onNext}>
+          <ArrowRightIcon color={'black'} />
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+NavBar.propTypes = {
+  firstMonthToDisplay: PropTypes.instanceOf(Date),
+  onNavigationClick: PropTypes.func,
+};
+
+NavBar.defaultProps = {
+  firstMonthToDisplay: moment().toDate(),
+  onNavigationClick: () => {},
+};
+
+export default NavBar;
