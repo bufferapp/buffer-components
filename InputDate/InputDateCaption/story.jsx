c8f7642046198f2f19b06fa8e3705e74b28bd54b
anafilipadealmeida <anafilipadealmeida@gmail.com>
2018-09-21 10:43:34 +0100

    Add prop date to caption's story.jsx

--- InputDate/InputDateCaption/story.jsx
+++ InputDate/InputDateCaption/story.jsx
@@ -2,10 +2,13 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { checkA11y } from 'storybook-addon-a11y';
-import Caption from './index'
+import Caption from './index';
+import moment from 'moment';
+
+const date = moment('2015-07-01').toDate();
 
 storiesOf('InputDate Caption', module)
   .addDecorator(checkA11y)
   .add('default', () => (
-    <Caption />
+    <Caption date={date}/>
   ));
