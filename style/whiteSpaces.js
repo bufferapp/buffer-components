d9038370b54c5cd5dda67ab2d8097fc00a4f6238
Karel McGrail <karel@buffer.com>
2018-09-21 14:28:38 +0100

    PUB-482 | Adding ability to wrap whitespace where necessary in the UI using a config flag

---
+++ style/whiteSpaces.js
@@ -0,0 +1,13 @@
+export const normal = 'normal';
+export const pre = 'pre';
+export const preLine = 'pre-line';
+export const preWrap = 'pre-wrap';
+export const noWrap = 'nowrap';
+
+export const whiteSpaces = [
+  normal,
+  pre,
+  preLine,
+  preWrap,
+  noWrap,
+];
